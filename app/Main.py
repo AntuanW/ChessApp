3870f26c6dbca09493aeba68d151f517f2e38131
Antoni <wojcikantek@gmail.com>
2023-05-29 12:07:00 +0200

    better structure

--- app/Main.py
+++ app/Main.py
@@ -3,11 +3,6 @@ from Modes import PvComputer, PvPLocal, PvPOnline
 from app.GUI.Auth.LoginWindow import LoginWindow
 
 if __name__ == "__main__":
-    #PvPLocal.game("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1") #wczytana gra gdzie ruch maja czarne
-    #PvPLocal.game("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2") #wczytana gra gdzie ruch maja białe
-    # PvComputer.game(chess.WHITE,2, "4k3/8/8/8/1Q5B/8/8/4K3 w - - 0 1")
-    #PvPLocal.game("4k3/8/8/8/8/8/8/4K3 w - - 0 1")
-    # PvPLocal.game()
     # LoginWindow().run()
     pass
 
--- tests/PVCTest.py
+++ tests/PVCTest.py
@@ -1,13 +1,8 @@
 import chess
-from app.Modes import PvComputer
 
-saves = [
-"rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1",   #0 czarne na ruchu
-"rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2", #1 białe na ruchu
-"rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2", #2 białe na ruchu
-"rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1",   #3 czarne na ruchu
-"4k3/8/8/8/1Q5B/8/8/4K3 w - - 0 1",                             #4 białe na ruchu, mat w 1
-]
+from app.Modes import PvComputer
+from saves import saves
+
 save = saves[4]
 our_color = chess.BLACK
 difficulty = 1
